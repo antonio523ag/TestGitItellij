f06af7b0e05b06caed50dd1486ef32c39f3f0460
Antonio Grillo <contactme@antoniogrillo.dev>
2026-09-18 09:25:43 +0200

    aggiunti i file Persona.java e Automobile.java

---
+++ src/Persona.java
@@ -0,0 +1,40 @@
+import java.time.LocalDate;
+
+public class Persona {
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+    private LocalDate dataNascita;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+
+    public LocalDate getDataNascita() {
+        return dataNascita;
+    }
+
+    public void setDataNascita(LocalDate dataNascita) {
+        this.dataNascita = dataNascita;
+    }
+}
