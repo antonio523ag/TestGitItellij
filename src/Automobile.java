913ebab9a3c682f362edc6ef957d1789d0b6d6f0
Antonio Grillo <contactme@antoniogrillo.dev>
2026-09-18 10:53:05 +0200

    aggiunti gli attributi alla classe Automobile.java

--- src/Automobile.java
+++ src/Automobile.java
@@ -1,2 +1,23 @@
+import java.time.LocalDate;
+
 public class Automobile {
+
+    private String targa;
+    private LocalDate dataImmatricolazione;
+
+    public String getTarga() {
+        return targa;
+    }
+
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+
+    public LocalDate getDataImmatricolazione() {
+        return dataImmatricolazione;
+    }
+
+    public void setDataImmatricolazione(LocalDate dataImmatricolazione) {
+        this.dataImmatricolazione = dataImmatricolazione;
+    }
 }
